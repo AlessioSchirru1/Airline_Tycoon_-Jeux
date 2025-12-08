68a733f22748e613b5590f0e6b188718d14a2c1e
Alessio Schirru <Alessio.Schirru@etu.univ-smb.fr>
2025-12-08 15:09:00 +0100

    Ajoutez des fichiers projet.

---
+++ Airline_Tycoon/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Airline_Tycoon
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
