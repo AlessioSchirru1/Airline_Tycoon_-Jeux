ad565bbc5f9945fda45594215dcc0a0ad93b3ed4
Raphael Savoie <Raphael.Savoie@etu.univ-smb.fr>
2025-12-16 10:45:57 +0100

    travail manager view

---
+++ Airline_Tycoon/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline_Tycoon
+{
+    public class Manager
+    {
+        public int Seats { get; set; } = 10;
+        public int SeatsPrice { get; set; } = 200;
+
+        public int Ticket { get; set; } = 18;
+        public int TicketPrice { get; set; } = 240;
+
+        public int Speed { get; set; } = 100;
+        public int SpeedPrice { get; set; } = 100;
+
+        public bool CanUpgradeSeats { get; set; }
+        public bool CanUpgradeSpeed { get; set; }
+        public bool CanUpgradeTickets { get; set; }
+    }
+
+}
--- Airline_Tycoon/ManagerView.xaml.cs
+++ Airline_Tycoon/ManagerView.xaml.cs
@@ -20,10 +20,36 @@ namespace Airline_Tycoon
     /// </summary>
     public partial class ManagerView : UserControl
     {
-        private List<Airplane> airplanes;
-        public ManagerView()
+        private List<Manager> manager;
+        public ManagerView(List<Manager> managerList)
         {
             InitializeComponent();
+            manager = managerList;
+
+            GenerateManagerViews();
+
+            var main = Application.Current.MainWindow as MainWindow;
+            main!.CapitalChanged += UpdateButtonsState;
+
+            UpdateButtonsState();
+        }
+        private void GenerateManagerViews()
+        {
+            ListContainer.Children.Clear();
+
+            int index = 1;
+            foreach (var plane in manager)
+            {
+                ListContainer.Children.Add(new AirplaneItem(manager, index));
+                index++;
+            }
+
+            // Bouton "Buy New Airplane" toujours en bas
+            ListContainer.Children.Add(AddManagerButton);
+
+            int nextIndex = airplanes.Count + 1;
+            NextAirplanePriceText.Text = $"${NumberFormatter.Format(GetAirplanePrice(nextIndex))}";
+
         }
     }
 }
