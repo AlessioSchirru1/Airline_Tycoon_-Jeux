5e7aa7e93518ce40555cf6b04dea7803ebb3c62f
Raphael Savoie <Raphael.Savoie@etu.univ-smb.fr>
2025-12-16 11:17:42 +0100

    travail manager view

--- Airline_Tycoon/Manager.cs
+++ Airline_Tycoon/Manager.cs
@@ -8,18 +8,14 @@ namespace Airline_Tycoon
 {
     public class Manager
     {
-        public int Seats { get; set; } = 10;
-        public int SeatsPrice { get; set; } = 200;
+        public double MultiplierSpeed { get; set; } = 1.00;
+        public int MultiplierSpeedPrice { get; set; } = 200;
 
-        public int Ticket { get; set; } = 18;
-        public int TicketPrice { get; set; } = 240;
+        public int  Multiplier  { get; set; } = 18;
+        public int MultiplierPrice { get; set; } = 240;
 
-        public int Speed { get; set; } = 100;
-        public int SpeedPrice { get; set; } = 100;
-
-        public bool CanUpgradeSeats { get; set; }
-        public bool CanUpgradeSpeed { get; set; }
-        public bool CanUpgradeTickets { get; set; }
+        public bool CanUpgradeMultiplierSpeed { get; set; }
+        public bool CanUpgradeMultiplier { get; set; }
     }
 
 }
